1b9003d1a8047586161cae1f037a92f7662ea566
Francisco Dans <fdansv@gmail.com>
2013-02-26 17:42:16 +0000

    main source files first commit

---
+++ src/com/dansd/jGeoHash/BoundingBox.java
@@ -0,0 +1,116 @@
+package com.dansd.jGeoHash;
+
+import java.util.ArrayList;
+
+public class BoundingBox {
+	double[] coords;
+	public double n,s,e,w;
+	String hash;
+	public BoundingBox(double north, double south, double east, double west){
+		//to do, build an approximate binary hash from a given bb
+		n=north;
+		s=south;
+		e=east;
+		w=west;		
+	}
+	public BoundingBox(String theHash){ 
+
+		hash=theHash;
+		n=90;
+		s=-90;
+		w=-180;
+		e=180;
+		fromHash();
+	}
+	public void fromHash(){
+		String hashCopy = hash;
+		while(hashCopy.equals("")==false){
+			//System.out.println(n +" "+s+" "+e+" "+w);
+			if(hashCopy.substring(0,1).equals("0")){
+				e=(e+w)/2;
+			}
+			else{
+				//System.out.println(hashCopy.substring(0,1));
+				w=(e+w)/2;
+			}
+			if(hashCopy.substring(1,2).equals("0")){
+				s=(n+s)/2;
+			}
+			else{
+				//System.out.println(hashCopy.substring(0,1));
+				n=(n+s)/2;
+			}
+			hashCopy=hashCopy.substring(2,hashCopy.length());
+		}
+	}
+
+	public ArrayList<String> surroundingBoxesList() {
+
+		ArrayList<String> theList = new ArrayList<String>();
+		int precision = 12;
+		try {
+			String NORTH = new GeoPoint(n + getHeight() / 2, (e + w) / 2,
+					precision).theHash;
+			theList.add(NORTH);
+		} catch (Error e) {
+		}
+		try {
+			String SOUTH = new GeoPoint(s - getHeight() / 2, (e + w) / 2,
+					precision).theHash;
+			theList.add(SOUTH);
+		} catch (Error e) {
+		}
+		try {
+			String NORTHEAST = new GeoPoint(n + getHeight() / 2,
+					(e + getWidth()) / 2, precision).theHash;
+			theList.add(NORTHEAST);
+		} catch (Error e) {
+		}
+		try {
+			String SOUTHEAST = new GeoPoint(s - getHeight() / 2,
+					(e - getWidth()) / 2, precision).theHash;
+			theList.add(SOUTHEAST);
+		} catch (Error e) {
+		}
+		try {
+			String NORTHWEST = new GeoPoint(n + getHeight() / 2,
+					(w - getWidth()) / 2, precision).theHash;
+			theList.add(NORTHWEST);
+		} catch (Error e) {
+		}
+		try {
+			String SOUTHWEST = new GeoPoint(s - getHeight() / 2,
+					(w - getWidth()) / 2, precision).theHash;
+			theList.add(SOUTHWEST);
+		} catch (Error e) {
+		}
+		try {
+			String EAST = new GeoPoint(n + s / 2, e + getWidth() / 2,
+					precision).theHash;
+			theList.add(EAST);
+		} catch (Error e) {
+		}
+		try {
+			String WEST = new GeoPoint(n + s / 2, w - getWidth() / 2,
+					precision).theHash;
+			theList.add(WEST);
+		} catch (Error e) {
+		}
+		theList.add(hash);
+		return theList;
+	}
+	public double getWidth(){
+		double width = w-e;
+		if(width<0){
+			width*=-1;
+		}
+		return width;
+	}
+	public double getHeight(){
+		double height = n-s;
+		if(height<0){
+			height*=-1;
+		}
+		return height;
+	}
+}
---
+++ src/com/dansd/jGeoHash/GeoPoint.java
@@ -0,0 +1,53 @@
+package com.dansd.jGeoHash;
+
+public class GeoPoint {
+	double pointLat;
+	double pointLon;
+	float normLat;
+	float normLon;
+	public String theHash;
+	int precision;
+	public GeoPoint(double lat,double lon, int prec){
+		if(lat>90 || lat<-90){
+			throw new Error("Latitude must be between -90 and 90 (got "+lat+" instead)");
+		}
+		if(lon>180 || lon<-180){
+			throw new Error("Longitude must be between -180 and 180 (got "+lon+" instead)");
+		}
+		pointLat= lat;
+		pointLon= lon;
+		precision = prec;
+		hash();
+	}
+	public void hash(){
+		int i = 0;
+		float northLimit = 90;
+		float eastLimit = 180;
+		float southLimit = -90;
+		float westLimit = -180;
+		String hashString = "";
+		while(i<precision){
+			if (pointLon<(eastLimit+westLimit)/2){
+				
+				hashString=hashString+"0";
+				eastLimit = (eastLimit+westLimit)/2;
+			}
+			else{
+				hashString=hashString+"1";
+				westLimit = (eastLimit+westLimit)/2;
+			}
+			if (pointLat<(northLimit+southLimit)/2){
+				
+				hashString=hashString+"1";
+				northLimit = (northLimit+southLimit)/2;
+			}
+			else{
+				hashString=hashString+"0";
+				southLimit = (northLimit+southLimit)/2;
+			}
+			i++;
+		}
+		theHash = hashString;
+	}
+	
+}
