035918d569efc9e23412fbe65eecb733c3ca0ff7
Francisco Dans <fdansv@gmail.com>
2013-09-09 14:19:42 +0100

    added a finished, very basic test

--- src/com/dansd/jGeoHash/GeoHashTests.java
+++ src/com/dansd/jGeoHash/GeoHashTests.java
@@ -1,9 +1,10 @@
 package com.dansd.jGeoHash;
 
 public class GeoHashTests {
-    public int main(String[] args){
+    public static void main(String[] args){
         GeoPoint point = new GeoPoint(51.52153, -0.1274, 10);
         System.out.println(point.getHash());
-        return 0;
+        BoundingBox bb = new BoundingBox(point.getHash());
+        System.out.println(bb.w + " " + bb.e + " " + bb.n + " " + bb.s);
     }
 }
