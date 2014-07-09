8c657c6df0998887b6ded936317fad29806f45e2
Francisco Dans <fdansv@gmail.com>
2014-07-09 19:00:27 +0100

    adds precision testing

--- src/test/java/com/dansd/jGeoHash/BoundingBoxTest.java
+++ src/test/java/com/dansd/jGeoHash/BoundingBoxTest.java
@@ -30,4 +30,21 @@ public class BoundingBoxTest {
         bb = new GeoPoint(100,50).getHashedBox(10);
         bb = new GeoPoint(50,200).getHashedBox(10);
     }
+    @Test
+    public void testControlDigit() throws Exception {
+        BoundingBox bb = new GeoPoint(51,1).getHashedBox(10);
+        assert bb.getHash().charAt(0) == '1';
+        bb = new GeoPoint(-40,1).getHashedBox(0);
+        assert bb.getHash().charAt(0) == '1';
+    }
+    @Test
+    public void testPrecisionValues() throws Exception{
+        BoundingBox bb = new GeoPoint(-21,20).getHashedBox(10);
+        assert bb.getPrecision()==10;
+    }
+    @Test
+    public void testPrecisionStress() throws Exception{
+        BoundingBox bb = new GeoPoint(39.9259500,-75.1196200).getHashedBox(22);
+        bb.printGeoJSON();
+    }
 }
