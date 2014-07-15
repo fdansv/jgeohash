ba1528778c6ecf4448e8f68883acba40312cd2c5
Francisco Dans <fdansv@gmail.com>
2014-07-15 11:54:36 +0100

    adds a couple of tests for hashed shape

--- src/test/java/com/dansd/jGeoHash/HashedShapeTest.java
+++ src/test/java/com/dansd/jGeoHash/HashedShapeTest.java
@@ -1,9 +1,45 @@
 package com.dansd.jGeoHash;
 
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class HashedShapeTest {
+    JSONObject object;
+    @Before
+    public void initPolygon(){
+        object = new JSONObject(file2String("london.json"));
+    }
+
+    @Test
+    public void geoJSONDisplayTest(){
+        Polygon polygon = new Polygon(object);
+        HashedShape hs = new HashedShape(polygon, 12);
+        assert hs.size()>0;
+        System.out.println("[");
+        for (BoundingBox bb: hs){
+            bb.printGeoJSON();
+            System.out.println(",");
+        }
+        System.out.println("]");
+
+    }
+
+    private String file2String(String path){
+        byte[] encoded = new byte[0];
+        try {
+            encoded = Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(encoded);
+    }
 
 }
